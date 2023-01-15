bfdf9e8d4990ad4339311620251b7ad31486a878
mesbahil <mesbahil@AG1115>
2023-01-15 17:00:40 +0330

    Add project files.

---
+++ ECommerceApp/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+
+namespace ECommerceApp.Client.Shared
+{
+    public partial class ProductList
+    {
+        private static List<Product> _products = new List<Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<List<Product>>("api/product");
+            if (result != null)
+                _products = result;
+        }
+    }
+}
